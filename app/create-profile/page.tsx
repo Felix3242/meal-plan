0b26b316957039bfb967eab161d45f1768a74de1
Felix Gao <llai92641@gmail.com>
2026-02-28 17:04:54 -0500

    finished redirect create-profile page

--- app/create-profile/page.tsx
+++ app/create-profile/page.tsx
@@ -1,5 +1,43 @@
+import { useUser } from "@clerk/nextjs";
+import { useMutation } from "@tanstack/react-query"
+import { useEffect } from "react";
+
+type ApiResponse = {
+    message: string;
+    error?: string;
+}
+
+async function createProfileRequest() {
+    const response = await fetch("/api/create-profile", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    const data = await response.json()
+    return data as ApiResponse
+}
+
 export default function CreateProfile() {
+    const {isLoaded, isSignedIn} = useUser();
+    const {mutate, isPending} = useMutation<ApiResponse, Error>({
+        mutationFn: createProfileRequest,
+        onSuccess: (data) => {
+            console.log(data)
+        },
+        onError: (error) => {
+            console.error(error)
+        }
+    });
+
+    useEffect(() => {
+        if (isLoaded && isSignedIn && !isPending) {
+            mutate()
+        }
+    }, [isLoaded, isPending, isSignedIn, mutate]);
+
     return (
-        <div></div>
+        <div> Processing sign in...</div>
     )
 }
