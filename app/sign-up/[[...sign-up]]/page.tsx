ab0eaaf23934e2d73a418e8b9b6ea6cedc9becd2
Felix Gao <llai92641@gmail.com>
2026-02-28 15:56:15 -0500

    homepage

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
+export default function SignUpPage() {
   return (
     <div className="px-4 py-8 sm:py-12 lg:py-16 max-w-7xl mx-auto flex justify-center align-center">
-      <SignUp forceRedirectUrl="/subscribe" />{" "}
+      <SignUp signInFallbackRedirectUrl="/subscribe" />{" "}
     </div>
   );
 }
