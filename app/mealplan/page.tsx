ae8d384be4baef158ac1d1cfe7fd5f6fd550b136
Felix Gao <llai92641@gmail.com>
2026-03-01 17:00:20 -0500

    finished meal plan outline

--- app/mealplan/page.tsx
+++ app/mealplan/page.tsx
@@ -1,4 +1,33 @@
+"use client";
+
+interface MealPlanInput {
+  dietType: string;
+  calories: number;
+  allergies: string;
+  cuisine: string;
+  snacks: string;
+  days?: number;
+}
+
 export default function MealPlanDashboard() {
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const payload: MealPlanInput = {
+
+      dietType: formData.get("dietType")?.toString() || "",
+      calories: Number(formData.get("calories")) || 2000,
+      allergies: formData.get("allergies")?.toString() || "",
+      cuisine: formData.get("cuisine")?.toString() || "",
+      snacks: formData.get("snacks")?.toString() || "",
+      days: 7, // default to 7 days
+    }
+
+    console.log(payload);
+  }
+
   return (
     <div>
       {" "}
@@ -7,12 +36,13 @@ export default function MealPlanDashboard() {
         <div>
           {" "}
           <h1> Meal Plan Generator </h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <label htmlFor="dietType"> Diet Type </label>
               <input
                 type="text"
                 id="dietType"
+                name="dietType"
                 required
                 placeholder="e.g. Vegetarian, Vegan, Keto, Mediterranean..."
               />
@@ -23,6 +53,7 @@ export default function MealPlanDashboard() {
               <input
                 type="number"
                 id="calories"
+                name="calories"
                 required
                 min={500}
                 max={15000}
@@ -35,6 +66,7 @@ export default function MealPlanDashboard() {
               <input
                 type="text"
                 id="allergies"
+                name="allergies"
                 required
                 placeholder="e.g. Dairy, Nuts, Gluten, None..."
               />
@@ -45,13 +77,14 @@ export default function MealPlanDashboard() {
               <input
                 type="text"
                 id="cuisine"
+                name="cuisine"
                 required
                 placeholder="e.g. Italian, Mexican, Asian, None..."
               />
             </div>
 
             <div>
-              <input type="checkbox" id="snacks" />
+              <input type="checkbox" id="snacks" name="snacks" />
               <label htmlFor="snacks"> Include Snacks </label>
             </div>
 
