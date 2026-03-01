352dc3ffe4e70a0d0a445c1f6eb7eeca5405ee41
Felix Gao <llai92641@gmail.com>
2026-02-28 22:11:10 -0500

    subscribe page template

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,3 +1,5 @@
+import { availablePlans } from "@/lib/plans";
+
 export default function Subscribe () {
     return <div> 
         <div>
@@ -10,9 +12,41 @@ export default function Subscribe () {
                 Get started on our weekly plan or upgrade to monthly or yearly when you&apos;re ready.
             </p>
         </div>
-
-        <div>
-
-        </div>
+            {availablePlans.map((plan, keys) => (
+                <div key={keys}>
+                    <div>
+                        {plan.isPopular && <p>Most Popular</p>}
+                        <h3>{plan.name}</h3>
+                        <p>
+                            <span> ${plan.amount}</span> <span>/{plan.interval}</span>
+                        </p>
+                        <p>{plan.description}</p>
+                        <ul>
+                            {plan.features.map((feature, key) => (
+                                <li key={key}>
+                                    <svg 
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="flex-shrink-0 w-6 h-6 text-emerald-500"
+                                    >
+                                    <polyline points="20 6 9 17 4 12" />    
+                                    </svg>
+                                    <span> {feature} </span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <button>
+                        Subscribe {plan.name}
+                    </button>
+                </div>
+            ))}
     </div>
 }
