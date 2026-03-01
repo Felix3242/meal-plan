6e738253c424289ca35d9c8eaf980bc26bbccd04
Felix Gao <llai92641@gmail.com>
2026-02-28 22:47:32 -0500

    created checkout api endpoint

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,19 +1,45 @@
+import { getPriceIdFromType } from "@/lib/plans";
 import { NextRequest, NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
 
 export async function POST(request: NextRequest) {
-    
-    const {planType, userId, email} = await request.json()
+    try {
+        const {planType, userId, email} = await request.json()
 
-    if(!planType || !userId || !email) {
-        return NextResponse.json({ error: "Plan type, user ID, and email are required" }, { status: 400 })
+        if(!planType || !userId || !email) {
+            return NextResponse.json({ error: "Plan type, user ID, and email are required" }, { status: 400 })
+        }
+
+        const allowedPlanTypes = ["week", "month", "year"]
+
+        if (!allowedPlanTypes.includes(planType)) {
+            return NextResponse.json({ error: "Invalid plan type" }, { status: 400 })
+        }
+
+        const priceId = getPriceIdFromType(planType)
+
+        if(!priceId) {
+            return NextResponse.json({ error: "Invalid price id" }, { status: 400 })
+        }
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+                {
+                    price: priceId,
+                    quantity: 1,
+                },
+            ],
+            customer_email: email,
+            mode: "subscription",
+            metadata: {clerkUserId: userId, planType},
+            success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/subscribe`,
+        });
+
+        return NextResponse.json({ url: session.url })
     }
-
-    const allowedPlanTypes = ["week", "month", "year"]
-
-    if (!allowedPlanTypes.includes(planType)) {
-        return NextResponse.json({ error: "Invalid plan type" }, { status: 400 })
+    catch {
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
     }
-
-    
-
 }
