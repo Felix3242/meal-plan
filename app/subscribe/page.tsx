389543656dee8a4f65c996dc2864f60cc924d9ca
Felix Gao <llai92641@gmail.com>
2026-02-28 22:04:49 -0500

    added plans

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,3 +1,18 @@
 export default function Subscribe () {
-    return <div> Subscribe Page </div>
+    return <div> 
+        <div>
+            {" "}
+            <h2>
+                Pricing 
+            </h2> {" "}
+            <p> 
+                {" "}
+                Get started on our weekly plan or upgrade to monthly or yearly when you&apos;re ready.
+            </p>
+        </div>
+
+        <div>
+
+        </div>
+    </div>
 }
