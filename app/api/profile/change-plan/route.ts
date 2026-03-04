b132651eba1dd09bbbbb138c07f120e55af0cc2f
Felix Gao <llai92641@gmail.com>
2026-03-03 21:25:28 -0500

    updatedsubscription

--- app/api/profile/change-plan/route.ts
+++ app/api/profile/change-plan/route.ts
@@ -1,37 +1,70 @@
 import { NextResponse, NextRequest } from "next/server";
 import { currentUser } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
+import { stripe } from "@/lib/stripe";
+import { getPriceIdFromType } from "@/lib/plans";
 
 export async function POST(request: NextRequest) {
-    try {
-        const clerkUser = await currentUser()
-        if (!clerkUser?.id) {
-            return NextResponse.json({ error: "Unauthorized" });
-        }
-
-        const { newPlan } = await request.json();
-
-        if (!newPlan) {
-            return NextResponse.json({ error: "New plan is required" }, { status: 400 });
-        }
-
-        const profile = await prisma.profile.findUnique({
-            where: {
-                userId: clerkUser.id,
-            },
-            select: {
-                subscriptionTier: true,
-                subscriptionActive: true,
-            }
-        });
-
-        if (!profile) {
-            return NextResponse.json({ error: "Profile not found" }, { status: 404 });
-        }
-
-        return NextResponse.json({ subscription: profile });
-
-    } catch {
-        return NextResponse.json({ error: "Internal Error" }, { status: 500 });
+  try {
+    const clerkUser = await currentUser();
+    if (!clerkUser?.id) {
+      return NextResponse.json({ error: "Unauthorized" });
     }
+
+    const { newPlan } = await request.json();
+
+    if (!newPlan) {
+      return NextResponse.json(
+        { error: "New plan is required" },
+        { status: 400 },
+      );
+    }
+
+    const profile = await prisma.profile.findUnique({
+      where: {
+        userId: clerkUser.id,
+      },
+    });
+
+    if (!profile) {
+      return NextResponse.json({ error: "Profile not found" }, { status: 404 });
+    }
+
+    if (!profile.stripeSubscriptionId) {
+      return NextResponse.json(
+        { error: "No active subscription found" },
+        { status: 400 },
+      );
+    }
+
+    const subscriptionId = profile.stripeSubscriptionId;
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+    const subscriptionItemId = subscription.items.data[0].id;
+
+    if (!subscriptionItemId) {
+      return NextResponse.json(
+        { error: "No active subscription found" },
+        { status: 404 },
+      );
+    }
+
+    const updatedSubscription = await stripe.subscriptions.update(
+      subscriptionId,
+      {
+        cancel_at_period_end: false,
+        items: [
+          {
+            id: subscriptionItemId,
+            price: getPriceIdFromType(newPlan),
+          },
+        ],
+        proration_behavior: "create_prorations",
+      },
+    );
+
+    return NextResponse.json({ subscription: profile });
+  } catch {
+    return NextResponse.json({ error: "Internal Error" }, { status: 500 });
+  }
 }
